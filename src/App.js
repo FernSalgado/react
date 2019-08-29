e55c97a3ec434263c50689bda7ce05870ad204d2
Fernando Salgado <fernando_salgado243@hotmail.com>
2019-08-29 18:59:53 -0300

    Digimon melhor que Pokemon

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch, Link} from "react-router-dom";
+import { 
+  Container,
+  AppBar,
+  Toolbar,
+  Typography,
+  Card,
+  CardContent,
+  Button 
+} from "@material-ui/core";
 
-import './App.css';
 
 import {DetalhesPage} from "./pages/detalhes";
 import {HomePage} from "./pages/home";
 import {NaoEncontradoPage} from "./pages/naoEncontrado";
+import './App.css';
 
 function App(props) {
+ 
   return (
     <BrowserRouter>
-      <Switch>
-        <Route component={HomePage} exact path="/"></Route>
-        <Route component={DetalhesPage} path="/detalhes"></Route>
-        <Route component={NaoEncontradoPage}></Route>  
-      </Switch>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/">
+            <Typography variant="h6">Home</Typography>
+            </Button>
+          <Button color="inherit" component={Link} to="/detalhes">
+            <Typography variant="h6">Detalhes</Typography>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Switch>
+          <Route component={HomePage} exact path="/"></Route>
+          <Route component={DetalhesPage} path="/detalhes"></Route>
+          <Route component={NaoEncontradoPage}></Route>  
+        </Switch>
+      </Container>
     </BrowserRouter>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,44 @@
-import React from "react";
-import logo from '../logo.svg';
+import React,{useEffect,useState} from "react";
+import {Card, CardContent,CardMedia, CardActionArea} from "@material-ui/core";
 
+function Digimon(props){
+  return (
+  <Card elevation={5} style={{margin:25,width:"20%",display:"inline-block"}}>
+    <CardActionArea>
+      <CardMedia component="img" src={props.img} alt={`Imagem do digimon ${props.name}`}/>
+    </CardActionArea>
+    <CardContent style={{textAlign:"center"}}>
+    <p>{props.name}</p>
+      <p>Level: {props.level}</p>
+    </CardContent>
+  </Card>
+  )
+}
 export function HomePage(){
-    return <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-          Alo, mundo
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  const [data,setData] = useState([]);
+  useEffect(function() {
+    fetch('https://digimon-api.herokuapp.com/api/digimon')
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      setData(response);
+      console.log("Data apos o fetch:",data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  } , [] );  
+  if (data.length > 0){
+    return (
+      <div>
+        {data.map(function(digimon){
+          return <Digimon key={digimon.name}{...digimon}/>
+        })}
+      </div>
+    );
+  }else{
+    return <p>Carregando...</p>;
+  }
+
 };
